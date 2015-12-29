aed9482983079621893f98c73a5a39acebb0c627
İbrahim Bilge <ibrahim.bilge@intra.local>
2015-12-29 20:10:40 +0200

    trying to use jquery step 1

---
+++ src/test/_jquery.js
@@ -0,0 +1,25 @@
+(function () {
+    "use strict";
+
+    var $ = require('jquery');
+    var http = require('http');
+    var env = require('node-jsdom').env;
+
+    //describe("Jquery Tests", function () {
+    //
+    //    it("loads html and parses it as dom", function () {
+    //        var options = {host: 'jquery.com', port: 80, path: '/'};
+    //
+    //        var html = '';
+    //        http.get(options, function (res) {
+    //            res.on('data', function (data) {
+    //                html += data;
+    //            }).on('end', function () {
+    //                var title = $(html).find('title').text();
+    //                console.log(title);
+    //            });
+    //        });
+        //});
+    //});
+
+}());
