5373a9acc79d6e39015543c02d647a05030a6f20
İbrahim Bilge <ibrahim.bilge@intra.local>
2015-12-29 18:54:52 +0200

    installed chai and mocha for testing

---
+++ src/test/test.js
@@ -0,0 +1,13 @@
+(function () {
+    "use strict";
+
+    var assert = require("chai").assert;
+
+    describe("Examples", function () {
+
+        it("this is an example", function () {
+            assert.equal(7, 7);
+        });
+    });
+
+}());
